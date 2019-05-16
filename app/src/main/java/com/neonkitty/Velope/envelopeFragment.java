4053cc49202fb84f6066c336201a7ba19387f3c3
Suzy Kersten <suzykersten72@outlook.com>
2019-05-15 23:40:34 -0500

    improve envelopeFragment view with balance and footer add envelope button

--- app/src/main/java/com/neonkitty/Velope/envelopeFragment.java
+++ app/src/main/java/com/neonkitty/Velope/envelopeFragment.java
@@ -8,6 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 
 public class envelopeFragment extends ListFragment {
@@ -21,8 +27,31 @@ public class envelopeFragment extends ListFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Envelopes, android.R.layout.simple_list_item_1);
-        setListAdapter(adapter);
+        ListView listView = getListView();
 
+        //add button to the bottom of the envelopes listView and add a listener to it
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
+        View footerView = inflater.inflate(R.layout.envelope_fragment_listview_footer, null, false);
+
+        footerView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEnveleope();
+            }
+        });
+
+        listView.addFooterView(footerView);
+
+        //just need to change where this items array comes from to make this work with dynamic data
+       String[] items = getResources().getStringArray(R.array.Envelopes);
+
+       //create the arrayAdapter view
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+    }
+
+    private void addEnveleope(){
+        Toast.makeText(getContext(), "Allen add AddEnvelope Activity", Toast.LENGTH_LONG).show();
     }
 }
